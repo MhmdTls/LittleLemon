4f652b5ef7cb6410bb86afe354bd1e8f5e2904f9
Mohammad Tlais <mohammadjawadtlais@gmail.com>
2025-10-28 20:19:40 +0200

    Implemented Menu API, Booking API, Djoser auth, and token-based security

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,6 +1,8 @@
 from rest_framework import generics, viewsets  # add viewsets here
 from .models import Menu, Booking             # import Booking model too
 from .serializers import MenuSerializer, BookingSerializer
+from rest_framework.permissions import IsAuthenticated
+
 
 # Handles GET (list all) and POST (create new)
 class MenuItemsView(generics.ListCreateAPIView):
@@ -16,3 +18,4 @@ class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
