767c3bd3fc6581436da54e404245f7472c9a42c1
Mohammad Tlais <mohammadjawadtlais@gmail.com>
2025-10-29 15:23:01 +0200

    Add requirements.txt for Render deployment

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -39,6 +39,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 ROOT_URLCONF = 'littlelemon.urls'
 
