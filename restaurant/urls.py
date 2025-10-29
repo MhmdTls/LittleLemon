462e95f0930b1dffc78e89d2e56dbaa8587fb9bb
Mohammad Tlais <mohammadjawadtlais@gmail.com>
2025-10-29 14:25:29 +0200

    Add requirements.txt for Render deployment

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,12 +1,9 @@
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 from . import views
 from rest_framework.authtoken.views import obtain_auth_token
 
-
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('restaurant.urls')),
+    path('', views.home, name='home'),  # your homepage
     path('menu/', views.MenuItemsView.as_view(), name='menu-list'),
     path('menu/<int:pk>/', views.SingleMenuItemView.as_view(), name='menu-detail'),
     path('api-token-auth/', obtain_auth_token),
