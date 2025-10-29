1df78426478298110d957f66923885a435e2cad7
Mohammad Tlais <mohammadjawadtlais@gmail.com>
2025-10-29 14:20:19 +0200

    Add requirements.txt for Render deployment

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,6 +1,8 @@
+from django.contrib import admin
 from django.urls import include, path
 from . import views
 from rest_framework.authtoken.views import obtain_auth_token
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
